bafd3b494b532139e98d3e5473afd064336dd11e
Krzysztof Wiatr <krzywiatr@gmail.com>
2017-09-18 14:29:36 +0200

    first version, still html + js

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+
+document.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 113) {
+        alert("coś się zjebało");
+    }
+});
+
+
+
+
+
+
+
+  });
